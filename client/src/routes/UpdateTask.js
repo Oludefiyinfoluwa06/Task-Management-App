101e1545d06d3611b3200d7c19993ff0b0222c19
Timothy <oludefiyinfoluwa06@gmail.com>
2024-01-29 01:50:45 +0100

    Task management app in progress

--- client/src/routes/UpdateTask.js
+++ client/src/routes/UpdateTask.js
@@ -13,13 +13,13 @@ const UpdateTask = () => {
     const navigate = useNavigate();
     let token;
     
-    useEffect(() => {
-        const user = JSON.parse(localStorage.getItem('user'));
+    const user = JSON.parse(localStorage.getItem('user'));
     
-        if (!user) navigate('/login');
-        
-        token = user.token;
-    }, [navigate]);
+    if (!user) {
+        navigate('/login');
+    }
+    
+    token = user.token;
 
     useEffect(() => {
         axios.get(`https://task-management-server-rho-ten.vercel.app/api/tasks/${id}`, {
