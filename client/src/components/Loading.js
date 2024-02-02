4d65e12c72c61ceba9926282a13d5bb36737b851
Timothy <oludefiyinfoluwa06@gmail.com>
2024-02-02 20:12:10 +0100

    Updates

--- client/src/components/Loading.js
+++ client/src/components/Loading.js
@@ -3,17 +3,17 @@ import React from 'react';
 const Loading = () => {
     return (
         <div class="loading">
-        <span style="--i:1;"></span>
-        <span style="--i:2;"></span>
-        <span style="--i:3;"></span>
-        <span style="--i:4;"></span>
-        <span style="--i:5;"></span>
-        <span style="--i:6;"></span>
-        <span style="--i:7;"></span>
-        <span style="--i:8;"></span>
-        <span style="--i:9;"></span>
-        <span style="--i:10;"></span>
-    </div>
+            <span style={{ '--i':1 }}></span>
+            <span style={{ '--i':2 }}></span>
+            <span style={{ '--i':3 }}></span>
+            <span style={{ '--i':4 }}></span>
+            <span style={{ '--i':5 }}></span>
+            <span style={{ '--i':6 }}></span>
+            <span style={{ '--i':7 }}></span>
+            <span style={{ '--i':8 }}></span>
+            <span style={{ '--i':9 }}></span>
+            <span style={{ '--i':10 }}></span>
+        </div>
     );
 }
 
