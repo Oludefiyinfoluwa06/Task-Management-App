e8c7443c0430d6849ff700b82b8d249af94361ea
Timothy <oludefiyinfoluwa06@gmail.com>
2024-02-03 19:04:25 +0100

    Version 1 Complete

--- client/src/routes/Home.js
+++ client/src/routes/Home.js
@@ -6,7 +6,7 @@ const Home = () => {
     return (
         <div className="hero">
             <div className="hero-text">
-                <h1>Task Manager</h1>
+                <h1>Ofto Task Manager</h1>
                 <p>
                     Welcome to Ofto Task Manager, your ultimate companion in conquering tasks and maximizing productivity! Seamlessly designed to simplify your life, Ofto Task Manager is not just a task manager; it's your personal productivity powerhouse. In the fast-paced world we live in, staying organized is the key to success.
                 </p><br />
