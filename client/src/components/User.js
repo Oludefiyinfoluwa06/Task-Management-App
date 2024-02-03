55dbdf3fa16996ac17b83be9845bf5b54d357069
Timothy <oludefiyinfoluwa06@gmail.com>
2024-02-03 18:05:30 +0100

    Almost done with the first version

--- client/src/components/User.js
+++ client/src/components/User.js
@@ -1,13 +1,13 @@
-import React, { useState } from 'react';
+import React from 'react';
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
-import { FaPen, FaTimes } from 'react-icons/fa';
+// import { FaPen, FaTimes } from 'react-icons/fa';
 import { Link } from 'react-router-dom';
 import profilePic from '../assets/profile-pic.png';
 
 const User = ({ profile }) => {
-    const [toggleModal, setToggleModal] = useState(false);
-    const [userProfile, setUserProfile] = useState(null);
+    // const [toggleModal, setToggleModal] = useState(false);
+    // const [userProfile, setUserProfile] = useState(null);
     const navigate = useNavigate();
     const user = JSON.parse(localStorage.getItem('user'));
 
@@ -38,41 +38,40 @@ const User = ({ profile }) => {
             });
     }
 
-    const handleToggleModal = () => {
-        setToggleModal(!toggleModal);
-    }
+    // const handleToggleModal = () => {
+    //     setToggleModal(!toggleModal);
+    // }
 
-    const handleCloseModal = () => {
-        setToggleModal(false);
-    }
+    // const handleCloseModal = () => {
+    //     setToggleModal(false);
+    // }
 
-    const handleUploadPic = async e => {
-        e.preventDefault();
+    // const handleUploadPic = async e => {
+    //     e.preventDefault();
 
-        const formData = new FormData();
-        formData.append('userProfile', userProfile);
+    //     const formData = new FormData();
+    //     formData.append('userProfile', userProfile);
 
-        try {
-            const res = await axios.post('https://task-management-server-rho-ten.vercel.app/api/profile/upload', formData,
-                {
-                    headers: {
-                        'Content-Type': 'multipart/form-data',
-                        Authorization: token
-                    }
-                });
-            console.log(res);
-        } catch (error) {
-            console.log(error);
-        }
-    }
+    //     try {
+    //         const res = await axios.post('https://localhost:5000/api/profile/upload', formData,
+    //             {
+    //                 headers: {
+    //                     Authorization: token
+    //                 }
+    //             });
+    //         console.log(res);
+    //     } catch (error) {
+    //         console.log(error);
+    //     }
+    // }
 
     return (
         <div className='profile'>
             <div className="profile-pic">
                 <img src={profilePic} alt="User Profile" />
-                <div className="pic-edit" onClick={handleToggleModal}>
+                {/* <div className="pic-edit" onClick={handleToggleModal}>
                     <FaPen />
-                </div>
+                </div> */}
             </div>
             <div className="info">
                 <div className="profile-info">
@@ -84,15 +83,15 @@ const User = ({ profile }) => {
                     <div className="action" onClick={() => handleDeleteProfile(profile.id)}>Delete profile</div>
                 </div>
             </div>
-            <div className={toggleModal ? "show-modal" : "hide-modal"}>
+            {/* <div className={toggleModal ? "modal show-modal" : "modal hide-modal"}>
                 <div className="closeModal" onClick={handleCloseModal}>
                     <FaTimes />
                 </div>
-                <form onSubmit={handleUploadPic} encType='multipart/form-data'>
+                <form onSubmit={handleUploadPic}>
                     <input type="file" name="userProfile" id="userProfile" onChange={e => setUserProfile(e.target.files[0])} />
                     <button>Upload</button>
                 </form>
-            </div>
+            </div> */}
         </div>
     );
 }
