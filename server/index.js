8ab0d8abc2a3dcb8c678ed2203e137e4f2560941
Timothy <oludefiyinfoluwa06@gmail.com>
2024-02-03 17:54:10 +0100

    More updates

--- server/index.js
+++ server/index.js
@@ -13,10 +13,7 @@ mongoose.connect(process.env.MONGO_URI)
     .then(() => console.log('Database connected'))
     .catch(err => console.log(err));
 
-app.use(cors({
-    origin: 'http://localhost:3000',
-    credentials: true,
-}));
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
